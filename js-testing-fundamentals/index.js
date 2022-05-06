c5cce366aa858e271aa6ea4c4be5e9871433d40d
allen <ywl_allen@163.com>
2022-05-06 22:46:10 +0800

    add comment

--- js-testing-fundamentals/index.js
+++ js-testing-fundamentals/index.js
@@ -1,9 +1,12 @@
-const add = (a, b) => a - b;
+const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
+// ...args 接收所有参数，展开传递给 add，add 方法会按声明的个数与顺序接收 ...args
+// 简洁方式使用 resolve
 const addAsync = (...args) => Promise.resolve(add(...args));
 
 const subtractAsync = (...args) =>
+  // new 方式使用 promise
   new Promise((resolve) =>
     setTimeout(() => {
       resolve(subtract(...args));
